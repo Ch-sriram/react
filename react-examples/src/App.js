d9129cd0692a88d143b3c76adb995b0033090da0
Chandrabhatta Sriram <sriram.ch1995@gmail.com>
2020-07-24 16:19:00 +0530

    Inner workings of `styled-components`

--- react-examples/src/App.js
+++ react-examples/src/App.js
@@ -1,9 +1,29 @@
 // Required Packages/Dependencies
 import React, { Component } from 'react';
 import './App.css';
+import styled from 'styled-components';
 
 // Custom Components
 import Person from './components/person/person.component';
+
+// we can always store our styled components in separate files
+// and import them to use them whenever we want to.
+// But for now, we will define the styled-component right here.
+
+const StyledButton = styled.button`
+  color: white;
+  background-color: green;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+  transition: all .2s;
+  &:hover {
+    background-color: lightgreen;
+    color: rgba(0, 0, 0, .9);
+  }
+`;
+
 
 class App extends Component {
   state = {
@@ -39,21 +59,22 @@ class App extends Component {
     this.setState({ showPersons: !this.state.showPersons });
   }
 
+  /**
+   * The styles that we add using the styled-components library
+   * doesn't add styling inline to the component, but it 
+   * generates the styles that we added into internally
+   * generated CSS class selectors, and then adds those CSS 
+   * class(s) to the head of the document and then just adds
+   * the appropriate class selector to the component we 
+   * actually generated from the `styled` object.
+   * 
+   * Therefore, styled-components do NOT generate inline 
+   * styles, but they automatically generate and manage regular
+   * CSS classes for us.
+   * 
+   */
+
   render() {
-    const style = {
-      color: "white",
-      backgroundColor: "green",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer",
-      transition: "all .2s",
-      // to use radium, we can add the `hover` state as follows
-      ':hover': {
-        backgroundColor: "lightgreen",
-        color: "rgba(0, 0, 0, .9)"
-      }
-    };
 
     let persons = null;
     
@@ -74,14 +95,14 @@ class App extends Component {
         </div>
       );
       
-      // this is how we dynamically change the styling
-      style.backgroundColor = "red";
+      // // this is how we dynamically change the styling
+      // style.backgroundColor = "red";
 
-      // making use of radium for assigning :hover rules
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'rgba(0, 0, 0, .9)'
-      };
+      // // making use of radium for assigning :hover rules
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'rgba(0, 0, 0, .9)'
+      // };
     }
 
     const classes = [];
@@ -94,14 +115,19 @@ class App extends Component {
       classes.push('bold');
     }
 
+    /**
+     * Now we can simply use a <StyledButton> instead of a 
+     * normal <button>. The <StyledButton> is defined above.
+     */
+
     return (
       <div className="App">
         <h1>This is a React Example</h1>
         <p className={classes.join(" ")}>This is really working!</p>
 
-        <button onClick={this.togglePersonsHandler} style={style}>
+        <StyledButton onClick={this.togglePersonsHandler}>
           Toggle Persons
-        </button>
+        </StyledButton>
         {persons}
       </div>
     );
