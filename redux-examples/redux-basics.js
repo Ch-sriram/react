0bc9671d9cff57d92029ae119a0c593b63a1eb62
Chandrabhatta Sriram <sriram.ch1995@gmail.com>
2020-08-20 17:45:33 +0530

    Dispatching Actions & Adding Logic for Actions in Reducer

--- redux-examples/redux-basics.js
+++ redux-examples/redux-basics.js
@@ -10,55 +10,70 @@
  * Subscription.
  */
 
-const redux = require('redux'); // import can't be used in Node
-const createStore = redux.createStore;  // redux.createStore() is a function, and we're storing the reference of that function in our createStore constant.
+const redux = require('redux'); // ES6 imports not allowed
+const createStore = redux.createStore;  // function reference
 
-/**
- * Before passing any state to the rootReducer, we make our
- * own initialState, which can have anything as an initial 
- * state value.
- */
 const initialState = { counter: 0, };
 
 // REDUCER
 /**
- * Reducer is a collection of a bunch of all the reducers at a
- * single place. The Reducer takes in a function with 2 
- * arguments. param1 is the `currentState` (known as `state`) 
- * and param2 is `action`. The function, depending on the 
- * `action`'s type, can update the old `state` and return the 
- * new `state`.
- * 
- * For now, we will keep it simple, and return the state we 
- * take in as param1.
+ * The actions defined below using store.dispatch() are to be
+ * reduced (i.e., logic for the action is to be defined yet)
+ * inside the rootReducer below, as shown;
  */
-const rootReducer = (state = initialState, action) => state;
+const rootReducer = (state = initialState, action) => {
+  // NOTE: We always make changes to the copy of the state -- Immutable Modification
+  if (action.type === 'INC_COUNTER') {
+    return {
+      ...state, // copy the old state as the new state
+      counter: state.counter + 1, // then add 1 to counter
+    }
+  }
+
+  if (action.type === 'ADD_COUNTER') {
+    return {
+      ...state, // copy the old state as the new state
+      counter: state.counter + action.value, // for ADD_COUNTER action, we specifically mentioned `value` property, and that, we can use it here.
+    }
+  }
+
+  return state;
+};
 
 // STORE -- to make a STORE, we import redux from 'redux'
-const store = createStore(rootReducer);  // needs a reducer to be passed to it, and so, the REDUCER instance has to be created before we create the STORE's instance.
+const store = createStore(rootReducer);  // param1: reducer
+console.log(store.getState());  // { counter: 0 }
 
 /**
- * We now have a store, with undefined state, as we haven't 
- * initialized the `state` passed to the `rootReducer()` with
- * any value. We can verify that by the following statement:
+ * Now, we want to use the store, not just for outputting it 
+ * with store.getState() method, but also dispatch an ACTION.
+ * 
+ * An ACTION is DISPATCHED by using the store.dispatch() 
+ * function, which takes in an `action` argument, which is a
+ * JS Object, with the the first field to be given as `type`, 
+ * and we can give any kind of subsequent fields; By 
+ * convention, `payload` is the field used to send any data 
+ * along with the ACTION, but we can use any name as shown in
+ * the example below.
+ * 
+ * The `type` field is really important because the `type` 
+ * mentioned inside store.dispatch() will later be used inside
+ * the rootReducer() to know what to do for a a particular
+ * ACTION's `type`.
+ * 
+ * We use all uppercase characters to name `type` by convention
  */
-console.log(store.getState());
+// DISPATCHING ACTION
+store.dispatch({ type: `INC_COUNTER` });  // Increases the counter by 1
+store.dispatch({ type: `ADD_COUNTER`, value: 10 }); // Increases the counter by `value`
 
-// DISPATCHING / ACTION
+console.log(store.getState());
 
 // SUBSCRIPTION
 
-
 /**
- * 
- * Run Command: `node redux-basics.js`
- * 
  * Outputs
  * -------
- * #1: When `state` in the rootReducer() is not passed with any default initialState
- *    undefined
- * 
- * #2: When `state` in the rootReducer() is passed with a default initialState (as shown above)
  *    { counter: 0 }
- * 
+ *    { counter: 11 }
  */
