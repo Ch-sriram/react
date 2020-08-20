1893a829d83081b0303fdbf7089ebbcfdcd8436c
Chandrabhatta Sriram <sriram.ch1995@gmail.com>
2020-08-20 19:51:21 +0530

    Adding Subscriptions

--- redux-examples/redux-basics.js
+++ redux-examples/redux-basics.js
@@ -16,64 +16,66 @@ const createStore = redux.createStore;  // function reference
 const initialState = { counter: 0, };
 
 // REDUCER
-/**
- * The actions defined below using store.dispatch() are to be
- * reduced (i.e., logic for the action is to be defined yet)
- * inside the rootReducer below, as shown;
- */
 const rootReducer = (state = initialState, action) => {
   // NOTE: We always make changes to the copy of the state -- Immutable Modification
   if (action.type === 'INC_COUNTER') {
     return {
       ...state, // copy the old state as the new state
-      counter: state.counter + 1, // then add 1 to counter
+      counter: state.counter + 1,
     }
   }
 
   if (action.type === 'ADD_COUNTER') {
     return {
       ...state, // copy the old state as the new state
-      counter: state.counter + action.value, // for ADD_COUNTER action, we specifically mentioned `value` property, and that, we can use it here.
+      counter: state.counter + action.value,
     }
   }
 
   return state;
 };
 
-// STORE -- to make a STORE, we import redux from 'redux'
+// STORE -- to make a `store`, we import redux from 'redux'
 const store = createStore(rootReducer);  // param1: reducer
 console.log(store.getState());  // { counter: 0 }
 
+// SUBSCRIPTION
 /**
- * Now, we want to use the store, not just for outputting it 
- * with store.getState() method, but also dispatch an ACTION.
+ * SUBSCRIPTIONs make sure that we don't have to call 
+ * `store.getState()` to get the current state's snapshot 
+ * inside the STORE, but also to inform the user, whenever we
+ * need to do something with the new state, because something
+ * in the old state, changed. Now, instead of guessing when
+ * a particular action is executed and reduced in the store,
+ * and then manually add our `console.log(store.getState())`
+ * statement, we can instead simply add a SUBSCRIPTION that 
+ * automatically gets called whenever an ACTION is Dispatched
+ * and reduced by the REDUCER.
  * 
- * An ACTION is DISPATCHED by using the store.dispatch() 
- * function, which takes in an `action` argument, which is a
- * JS Object, with the the first field to be given as `type`, 
- * and we can give any kind of subsequent fields; By 
- * convention, `payload` is the field used to send any data 
- * along with the ACTION, but we can use any name as shown in
- * the example below.
+ * NOTE: SUBSCRIPTIONS should always be defined before the 
+ * ACTION DISPATCHERS.
  * 
- * The `type` field is really important because the `type` 
- * mentioned inside store.dispatch() will later be used inside
- * the rootReducer() to know what to do for a a particular
- * ACTION's `type`.
- * 
- * We use all uppercase characters to name `type` by convention
+ * `store.subscribe()` method doesn't take any arguments.
+ * We can return anything, or we can simply log the current
+ * state as shown below. The SUBSCRIPTION is triggered whenever
+ * an ACTION get DISPATCHED & reduced using the REDUCER.
  */
+
+store.subscribe(() => {
+  console.log("[Subscription]", store.getState());
+});
+
 // DISPATCHING ACTION
 store.dispatch({ type: `INC_COUNTER` });  // Increases the counter by 1
 store.dispatch({ type: `ADD_COUNTER`, value: 10 }); // Increases the counter by `value`
 
 console.log(store.getState());
 
-// SUBSCRIPTION
-
 /**
- * Outputs
- * -------
+ * Output
+ * ------
  *    { counter: 0 }
+ *    [Subscription] { counter: 1 }
+ *    [Subscription] { counter: 11 }
  *    { counter: 11 }
  */
