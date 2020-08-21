3503e30759d57a3ab8998386c5aa2022d79fbe6c
Chandrabhatta Sriram <sriram.ch1995@gmail.com>
2020-08-21 13:34:29 +0530

    Assignment Solution: Dispatching the Remaining ACTIONs

--- redux-examples/src/containers/Counter/Counter.js
+++ redux-examples/src/containers/Counter/Counter.js
@@ -7,21 +7,9 @@ import CounterControl from "../../components/CounterControl/CounterControl";
 import CounterOutput from "../../components/CounterOutput/CounterOutput";
 
 /**
- * Now, in our <Counter /> container, how do we access the
- * Redux STORE?
- *
- * For that, we need to connect this container with the Redux
- * STORE, or, to be precise, in the end what want to do is, we
- * want to setup a SUBSCRIPTION in this container.
- *
- * We connect the Redux STORE to this container, a bit
- * differently compared to how we did using Vanilla JS.
- *
- * We won't connect the Redux STORE to this container manually
- * by calling the store.subscribe() method, but instead, we'll
- * use a feature provided by 'react-redux' package, which is
- * known as `{ connect } from 'react-redux'` as imported above.
- *
+ * ASSIGNMENT 
+ * ----------
+ * Dispatch ACTIONS for DECREMENT, SUBTRACT 5 & ADD by 5.
  */
 
 class Counter extends Component {
@@ -65,78 +53,31 @@ class Counter extends Component {
         />
         <CounterControl
           label="Decrement"
-          clicked={() => this.counterChangedHandler("dec")}
+          clicked={this.props.onDecrementCounter}
         />
         <CounterControl
           label="Add 5"
-          clicked={() => this.counterChangedHandler("add", 5)}
+          clicked={this.props.onAddCounter}
         />
         <CounterControl
           label="Subtract 5"
-          clicked={() => this.counterChangedHandler("sub", 5)}
+          clicked={this.props.onSubtractCounter}
         />
       </div>
     );
   }
 }
 
-/**
- * `connect` is a HOC, which we use to wrap it around this
- * container (i.e., <Counter /> Component) when we export this
- * container (or, component). However, we won't wrap the
- * component directly, because `connect` itself is a function
- * that returns a function, and that returned function itself
- * takes the respective component/container as a parameter.
- *
- * Therefore, `connect` is a function that returns a HOC, which
- * means, `connect` is a closure HOC. And so, we pass in 2
- * vital pieces of information before `connect` actually wraps
- * around the actual container/component.
- *
- * The 2 pieces of information we send in as the the first set
- * of parameters to the `connect` closure HOC are:
- *  1. Which part of the whole application state is interesting
- *     to us? (Here, we are only interested in `counter`, from
- *     the state of this application. For a bigger app, we
- *     might want a different slice of the state)
- *  2. Which ACTIONs do we want to DISPATCH? (In a bigger app,
- *     we may have thousands of ACTIONs Dispatched from all
- *     over the application, but a given individual container
- *     may only DISPATCH a couple of ACTIONs)
- *
- * 2 =>
- * To Dispatch the ACTIONs, we, by convention, pass in a second
- * param to the first set of params being sent to the `connect`
- * closure HOC. The second param we send, is by convention
- * known as `mapDispatchToProps` constant, which is a function
- * which takes (by convention, `dispatch` as an argument) in
- * another function known as `dispatch` which is connected to
- * STORE's state, and will call `store.dispatch()` on the
- * STORE behind the scenes.
- *
- * The `mapDispatchToProps` method, takes in the `dispatch`
- * function as the param and returns an object in which each
- * field has property to which the value is an anonymous
- * function, which returns a call to the `dispatch()` function
- * passed to `mapDispatchToProps` method. Note that we can name
- * our property anyway we want.
- *
- * The `dispatch()` function inside the anonymous function
- * takes in a JS Object in which we have to definitely define
- * the name of the `task`, and then, if we have any `payload`,
- * in that case, we can also define the `payload` (i.e., any
- * data related to the action).
- *
- * The entire example of `mapDispatchToProps` is shown below.
- */
-
 const mapDispatchToProps = dispatch => {
   /**
-   * NOTE that we have to handle the INCREMENT action in the
-   * [store/reducer.js] file. Open that file to understand.
+   * NOTE that we have to handle all the dispatched ACTIONs in 
+   * the [store/reducer.js] file. Open that file to understand.
    */
   return {
-    onIncrementCounter: () => dispatch({ type: "INCREMENT" }), // we use this onIncrementCounter property/method as a prop in line #64
+    onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
+    onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
+    onAddCounter: () => dispatch({type: "ADD", value: 5}),
+    onSubtractCounter: () => dispatch({type: "SUBTRACT", value: 5}),
   };
 };
 
@@ -148,10 +89,3 @@ export default connect(mapStateToProps, mapDispatchToProps)(Counter);
 
 // In case of the component being a dumb component.
 // export default connect(null, mapDispatchToProps)(Counter);
-
-/**
- * For now, the 'INCREMENT' ACTION bound to the "Increment" 
- * Button in view, is the only button that's functioning, rest
- * all aren't functioning for now, as now, we've shifted state 
- * management to Redux from React.
- */
--- redux-examples/src/store/reducer.js
+++ redux-examples/src/store/reducer.js
@@ -1,10 +1,13 @@
 const initialState = { counter: 0 };
 
 const reducer = (state = initialState, action) => {
-  if (action.type === "INCREMENT") {
-    return { counter: state.counter + 1, }
+  switch (action.type) {
+    case "INCREMENT": return { counter: state.counter + 1, };
+    case "DECREMENT": return { counter: state.counter - 1, };
+    case "ADD": return { counter: state.counter + action.value, };
+    case "SUBTRACT": return { counter: state.counter - action.value, };
+    default: return state;
   }
-  return state;  
 };
 
 export default reducer;
