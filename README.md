59d1684d70d57c98bf9554a1989cdf2ee3bd61bd
Chandrabhatta Sriram <sriram.ch1995@gmail.com>
2020-08-27 13:01:49 +0530

    Using Utility Functions: Inside the Results REDUCER

--- README.md
+++ README.md
@@ -189,4 +189,6 @@ Required Dependency: **`npm i --save axios`** | **[`axios` DOCS](https://github.
 5. Restructuring **`ACTIONS`**: [Commit Details](https://github.com/Ch-sriram/react/commit/cf8c0dbdb42dea88c022fb6959a876d7f6275d18)
 6. Where to Put Data Transforming Logic? [Commit Details](https://github.com/Ch-sriram/react/commit/a771737aaa42d3070825b0786c0fae2dffc63445)
 7. Using **`ACTION Creators`** & `getState()`: [Commit Details](https://github.com/Ch-sriram/react/commit/87687d247d3bbd03afb64997e89e19499028f5c1)
-8. Using Utility Functions: [Commit Details](https://github.com/Ch-sriram/react/commit/b85404dc374ee3a722ba6ee38b1e4c583cf4209e)
+8. Using Utility Functions
+   1. Inside the **`Counter REDUCER`**: [Commit Details](https://github.com/Ch-sriram/react/commit/b85404dc374ee3a722ba6ee38b1e4c583cf4209e)
+   2. Inside the **`Results REDUCER`**: [Commit Details]()
--- redux-examples/src/store/reducers/results.js
+++ redux-examples/src/store/reducers/results.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 
 // handling only `results[]` here
 const initialState = { results: [] };
@@ -6,25 +7,11 @@ const initialState = { results: [] };
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
-        results: state.results.concat({
-          id: new Date().getUTCMilliseconds(),
-          // to access state.counter, we've to get it through
-          // the `action` object while dispatching this ACTION.
-          // value: state.counter,
-          value: action.counter * 2,
-        }),
-      };
+      return updateObject(state, { results: state.results.concat({ id: new Date().valueOf(), value: action.counter * 2, }) });
 
     case actionTypes.DELETE_RESULT:
-      const updatedResults = state.results.filter(
-        (result) => result.id !== action.resultElementID
-      );
-      return {
-        ...state,
-        results: updatedResults,
-      };
+      const updatedResults = state.results.filter(result => result.id !== action.resultElementID);
+      return updateObject(state, { results: updatedResults });
 
     default: return state;
   }
